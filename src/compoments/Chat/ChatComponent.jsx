9992aca0be04edecc5baba81211aa1e710117ed9
QMHLO <htetlinoo@qualymm.com>
2023-07-18 09:42:23 +0630

    chat fixing

--- src/compoments/Chat/ChatComponent.jsx
+++ src/compoments/Chat/ChatComponent.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { useMultiChatLogic, MultiChatSocket, MultiChatWindow, ChatHeader } from "react-chat-engine-advanced";
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced";
 
 const ChatComponent = ({ user }) => {
-  console.log(user);
   const projectId = process.env.REACT_APP_CE_PROJECT_ID;
   const username = user.username;
   const secret = user.email;
-  const first_name = user.first_name;
   const chatProps = useMultiChatLogic(projectId, username, secret);
   return (
     <div>
-      <MultiChatWindow {...chatProps} first_name={first_name} style={{ height: "100vh" }} />
+      <MultiChatWindow {...chatProps} style={{ height: "100vh" }} />
       <MultiChatSocket {...chatProps} />
-      {/* <ChatHeader title={first_name} /> */}
     </div>
   );
 };
